e3c174552603d2f8e1111ed4619778fac71ebebc
Starenesha <olgastarynenko@gmail.com>
2019-02-10 23:36:45 +0200

    Introduction to Python

---
+++ Introduction/Arithmetic Operators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c_sum = a + b
+    c_sub = a - b
+    c_prod = a * b
+    print(c_sum)
+    print(c_sub)
+    print(c_prod)
