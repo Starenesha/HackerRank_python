3b9fb4a6d5a5f5720cb6f95e3603c365c4116c13
Starenesha <olgastarynenko@gmail.com>
2019-02-11 15:45:11 +0200

    Find the runner-up score

---
+++ Basic Data Types/Find the Runner-Up Score.py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+    i = int(input())
+    lis = list(map(int,input().strip().split()))[:i]
+    z = max(lis)
+    while max(lis) == z:
+        lis.remove(max(lis))
+    print (max(lis))
